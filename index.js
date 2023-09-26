2752bd6a7d1665e9ad0b219c3290ee9bc9860947
Well-appedidos <wellington.lira@appedidos.com.br>
2023-09-26 17:51:23 -0300

    0.0.1

---
+++ index.js
@@ -0,0 +1,60 @@
+//
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const Sequelize = require("sequelize");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname+'/public'))
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+});
+
+sequelize.authenticate().then(function() {
+  console.log("Conectado com sucesso!");
+}).catch(function(erro) {
+  console.log("Falha ao conectar: " + erro);
+});
+
+const Sensor = sequelize.define("sensors", {
+  temperatura: {
+    type: Sequelize.INTEGER
+  },
+  umidade: {
+    type: Sequelize.INTEGER
+  }
+});
+
+app.get("/", function(req, res) {
+    const temperatura = req.query.temperatura;
+    const umidade = req.query.umidade;
+
+});
+
+app.get("/cadastrar", function (req, res){
+    const temperatura = req.query.temperatura;
+    const umidade = req.query.umidade;
+
+  Sensor.create({
+    temperatura: temperatura,
+    umidade: umidade
+  })
+    .then(() => {
+      console.log("Dados cadastrados com sucesso!");
+      res.redirect("/");
+    })
+    .catch(error => {
+      console.error("Erro ao cadastrar os dados:", error);
+      res.redirect("/");
+    });
+});
+
+app.listen(8080, function() {
+  console.log("Servidor rodando na porta 8080");
+});
